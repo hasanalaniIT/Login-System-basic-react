e287111af844f4d21014b366c32b3d53653dc2da
Hasan <hasanalaniIT@users.noreply.github.com>
2021-08-21 18:47:46 +0300

    Using react context before use context hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState ,useEffect } from 'react';
-
+import AuthContext from './Store/Auth-context'; 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -28,11 +28,13 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, }}>
+      <MainHeader  onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
