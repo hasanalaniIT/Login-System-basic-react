b910eb7591478efb91713595ea71079a03875e30
Hasan <hasanalaniIT@users.noreply.github.com>
2021-08-21 19:01:57 +0300

    Using UseContext hook for logout and log in

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { useState ,useEffect } from 'react';
-import AuthContext from './Store/Auth-context'; 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import React, { useState, useEffect } from "react";
+import AuthContext from "./Store/Auth-context";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
- 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
- 
+
   useEffect(() => {
-     const userLoggedIn=localStorage.getItem('isLoggedIn');
-  if (userLoggedIn==='1') {
-    setIsLoggedIn(true);
-  }
-  },[]);
+    const userLoggedIn = localStorage.getItem("isLoggedIn");
+    if (userLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn','1' );
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
   return (
     <React.Fragment>
-      <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, }}>
-      <MainHeader  onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+      >
+        <MainHeader />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
       </AuthContext.Provider>
     </React.Fragment>
   );
